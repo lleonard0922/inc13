17420a5ace465828cd5997eb07428d26444651bd
lleonard0922 <lleonard12@student.gsu.edu>
2026-04-23 12:05:20 -0400

    Create list.jsx

---
+++ src/list.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map((item) => (
+          <li key={item.name}>
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default List;
